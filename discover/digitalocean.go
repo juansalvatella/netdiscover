3334e8839d265b506e242cc425b06a0eaa4aed64
Joan Salvatella <joan@bookline.io>
2020-07-10 18:29:03 +0200

    added private ip

--- discover/digitalocean.go
+++ discover/digitalocean.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	doHostnameURL = "http://169.254.169.254/metadata/v1/hostname"
-	//doPrivateIPv4URL = "http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address"
+	doPrivateIPv4URL = "http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address"
 	doPublicIPv4URL = "http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address"
 	//doFloatingPublicIPv4EnabledURL = "http://169.254.169.254/metadata/v1/floating_ip/ipv4/active"
 	//doFloatingPublicIPv4URL        = "http://169.254.169.254/metadata/v1/interfaces/public/0/anchor_ipv4/address"
@@ -20,6 +20,7 @@ func NewDigitalOceanDiscoverer() Discoverer {
 	return NewDiscoverer(
 		PublicHostnameDiscovererOption(doHostname),
 		PublicIPv4DiscovererOption(doPublicIPv4),
+		PrivateIPv4DiscovererOption(doPrivateIPv4),
 		PublicIPv6DiscovererOption(doPublicIPv6),
 	)
 
@@ -29,12 +30,9 @@ func doHostname() (string, error) {
 	return StandardHostnameFromHTTP(doHostnameURL, nil)
 }
 
-// FIXME:  this URL never seems to be populated
-/*
 func doPrivateIPv4() (net.IP, error) {
 	return StandardIPFromHTTP(doPrivateIPv4URL, nil)
 }
-*/
 
 func doPublicIPv4() (net.IP, error) {
 	return StandardIPFromHTTP(doPublicIPv4URL, nil)
